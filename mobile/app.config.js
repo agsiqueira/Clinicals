9e253d24f0dd19c2d49e677b6e3f4604fb7c292e
AGSiqueira <agomesdesiqueira@ufl.edu>
2026-06-15 20:57:58 -0400

    Configure EAS Android build

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -4,6 +4,10 @@ export default ({ config }) => ({
   ...config,
   plugins: Array.from(new Set([...(config.plugins || []), "expo-video"])),
   extra: {
+    ...(config.extra || {}),
     API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
+    eas: {
+      projectId: "245a4a98-9645-43d9-9664-9d34c502e8eb",
+    },
   },
 });
